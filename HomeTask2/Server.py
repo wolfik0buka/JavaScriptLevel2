b4a1e733dea495cc40d788ea2132e2b9dc9bc28e
Egior <molodoi_padavan@mail.ru>
2018-08-13 13:42:36 +0300

    Hometask2 Ended

--- HomeTask2/Server.py
+++ HomeTask2/Server.py
@@ -1,38 +1,85 @@
 import json
-
 from http.server import BaseHTTPRequestHandler,HTTPServer
 
+#
+# menu = [{'name': "Личный кабинет", 'href': '#'},
+#         {'name': "Каталог", 'submenu': [{'name': "Один", 'href': '#'},
+#                                         {'name': "Два", 'href': '#'},
+#                                         {'name': "Три", 'href': '#'},
+#                                         {'name': "А тут много",'submenu': [{'name': "Много Один", 'href': '#'},
+#                                                                            {'name': "Много Два", 'href': '#'},
+#                                                                            {'name': "Много Три", 'href': '#'}]
+#                                          }
+#                                         ]
+#          },
+#         {'name': "Промоакции",  'submenu': [{'name': "ПромоОдин", 'href': '#'},
+#                                             {'name': "ПромоДва", 'href': '#'},
+#                                             {'name': "ПромоТри", 'href': '#'}]
+#          }]
 
-menu = [{'name': "Личный кабинет", 'href': '#'},
-        {'name': "Каталог", 'submenu': [{'name': "Один", 'href': '#'},
-                                        {'name': "Два", 'href': '#'},
-                                        {'name': "Три", 'href': '#'},
-                                        {'name': "А тут много",'submenu': [{'name': "Много Один", 'href': '#'},
-                                                                           {'name': "Много Два", 'href': '#'},
-                                                                           {'name': "Много Три", 'href': '#'}]
-                                         }
-                                        ]
-         },
-        {'name': "Промоакции",  'submenu': [{'name': "ПромоОдин", 'href': '#'},
-                                            {'name': "ПромоДва", 'href': '#'},
-                                            {'name': "ПромоТри", 'href': '#'}]
-         }]
-print (json.dumps(menu))
+
+#galery = [{'preview' :'путь миниатюра', 'href': 'Ссылка на полноразмерную картинку'}, ]
+galery = [{'preview': 'img/macro1preview.jpg', 'href': 'img/fullsize/macro1.jpg'},
+          {'preview': 'img/macro2preview.jpg', 'href': 'img/fullsize/macro2.jpg'},
+          {'preview': 'img/macro3preview.jpg', 'href': 'img/fullsize/macro3.jpg'},
+          {'preview': 'img/macro4preview.jpg', 'href': 'img/fullsize/macro4.jpg'},
+          {'preview': 'img/macro5preview.jpg', 'href': 'img/fullsize/macro5.jpg'},
+          {'preview': 'img/macro6preview.jpg', 'href': 'img/fullsize/macro6.jpg'},
+          {'preview': 'img/macro7preview.jpg', 'href': 'img/fullsize/macro7.jpg'},
+          {'preview': 'img/macro8preview.jpg', 'href': 'img/fullsize/macro8.jpg'},
+          {'preview': 'img/macro9preview.jpg', 'href': 'img/fullsize/macro9.jpg'}]
+
+
+
+
+def getMenu():
+    #***Генерация меню***
+    menu = [{'name': "Личный кабинет", 'href': '#'},
+            {'name': "Каталог", 'submenu': [{'name': "Один", 'href': '#'},
+                                            {'name': "Два", 'href': '#'},
+                                            {'name': "Три", 'href': '#'},
+                                            {'name': "А тут много", 'submenu': [{'name': "Много Один", 'href': '#'},
+                                                                                {'name': "Много Два", 'href': '#'},
+                                                                                {'name': "Много Три", 'href': '#'}]
+                                             }
+                                            ]
+             },
+            {'name': "Промоакции", 'submenu': [{'name': "ПромоОдин", 'href': '#'},
+                                               {'name': "ПромоДва", 'href': '#'},
+                                               {'name': "ПромоТри", 'href': '#'}]
+             }]
+
+    return json.dumps(menu)
+
+def getGalery():
+    galery = [{'preview': 'img/macro1preview.jpg', 'href': 'img/fullsize/macro1.jpg'},
+              {'preview': 'img/macro2preview.jpg', 'href': 'img/fullsize/macro2.jpg'},
+              {'preview': 'img/macro3preview.jpg', 'href': 'img/fullsize/macro3.jpg'},
+              {'preview': 'img/macro4preview.jpg', 'href': 'img/fullsize/macro4.jpg'},
+              {'preview': 'img/macro5preview.jpg', 'href': 'img/fullsize/macro5.jpg'},
+              {'preview': 'img/macro6preview.jpg', 'href': 'img/fullsize/macro6.jpg'},
+              {'preview': 'img/macro7preview.jpg', 'href': 'img/fullsize/macro7.jpg'},
+              {'preview': 'img/macro8preview.jpg', 'href': 'img/fullsize/macro8.jpg'},
+              {'preview': 'img/macro9preview.jpg', 'href': 'img/fullsize/macro9.jpg'}]
+    return json.dumps(galery)
+
 
 class HttpProcessor(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
-        if self.path == '/hello':
-
-            self.send_header('content-type','application/json')
-            self.send_header('Access-Control-Allow-Origin', '*')
-            self.end_headers()
-            self.wfile.write(bytes(json.dumps(menu), 'utf8'))
-            print(self.headers)
-            print(self.path)
+        if self.path == '/getMenu':
+            data = getMenu()
+        elif self.path == '/getGalery':
+            data = getGalery()
         else:
             print(self.path)
             print('Unknown API')
+            return 0
+        self.send_header('content-type','application/json')
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.end_headers()
+        self.wfile.write(bytes(data, 'utf8'))
+
 
 
 serv = HTTPServer(("localhost",8000),HttpProcessor)
--- HomeTask2/main.js
+++ HomeTask2/main.js
@@ -57,34 +57,152 @@ function SubMenu(name, items){
 const createMenu = function(base){
     let menuStruct = JSON.parse(base);
     let menu = new Menu(null, null, menuStruct);
-    let body = document.querySelector('body');
-    body.appendChild(menu.render());
+    let menuSection = document.querySelector('section.menu');
+    menuSection.innerHTML = '';
+    menuSection.appendChild(menu.render());
+};
+
+const createGalery = function (rowData) {
+    let parrent = document.querySelector("section.galery");
+    parrent.innerHTML = '';
+    let data ='';
+    let listImages = JSON.parse(rowData);
+    for (let image of listImages ){
+
+        let item = document.createElement('a');
+        item.href = image.href;
+        item.classList.add('galery-item');
+        item.innerHTML = `<img src="${image.preview}" alt="Galery Item">`;
+        parrent.appendChild(item);
+    }
+};
+
+var createUserList = function(data){
+    let parrent = document.querySelector(".list-user");
+    parrent.innerHTML = '';
+    for (let user of data){
+        let userDiv = document.createElement('div');
+        userDiv.classList.add('user');
+        userDiv.innerHTML = `<p><span>Name: </span>${user.name}</p>
+                             <p><span>E-mail: </span><a href="mailto:${user.email}">${user.email}</a></p>
+                             <p><span>Age: </span>${user.age}</p>`;
+        parrent.appendChild(userDiv);
+    }
+};
+
+var createUser = function(data){
+    let parrent = document.querySelector(".selectedUser");
+    parrent.innerHTML = '';
+    if (data.hasOwnProperty('name')){
+        let userDiv = document.createElement('div');
+        userDiv.classList.add('user');
+        userDiv.innerHTML = `<p><span>Name: </span>${data.name}</p>
+                             <p><span>E-mail: </span><a href="mailto:${data.email}">${data.email}</a></p>
+                             <p><span>Age: </span>${data.age}</p>`;
+        parrent.appendChild(userDiv);
+    }
+    else {
+        let error = document.createElement('p');
+        error.innerText = 'Такого пользователя не существует';
+        parrent.appendChild(error);
+    }
 };
 
 
-var testAjax = function () {
-
+var getMenu = function () {
     xhr = new XMLHttpRequest();
-    xhr.open('GET', 'http://localhost:8000/hello',true);
-
+    xhr.open('GET', 'http://localhost:8000/getMenu',true);
     xhr.onreadystatechange = function(){
-
         if(xhr.readyState !== 4) return;
-
         if(xhr.status !== 200){
             console.error(xhr.status)
         } else {
             createMenu(xhr.responseText);
         }
     };
-
     xhr.send();
 };
 
+var getGalery = function () {
+    xhr = new XMLHttpRequest();
+    xhr.open('GET', 'http://localhost:8000/getGalery');
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState !== 4) return;
+        if(xhr.status !== 200){
+            console.error(xhr.status)
+        } else {
+            createGalery(xhr.responseText);
+        }
+    };
+    xhr.send();
+};
 
+var getUserList = function(){
+    fetch('http://89.108.65.123/user').then((res) => {
+       return res.json();
+    }).then((data) =>{
+        createUserList(data);
+    }).catch((err) =>{
+        console.error(err);
+    })
+};
+var getUserId = function(){
+    let idUser = document.querySelector("#id-user");
+    fetch(`http://89.108.65.123/user/${idUser.value}`).then((res) => {
+        return res.json();
+    }).then((data) =>{
+        createUser(data);
+    }).catch((err) =>{
+        console.error(err);
+    });
+};
+var notifySucsess = function () {
+    let notify = document.createElement("p");
+    notify.classList.add('notify');
+    notify.innerText = "Пользователь успешно добавлен";
+    let parent = document.querySelector("div.notify");
+    parent.appendChild(notify);
+    setTimeout(() =>{
+        parent.removeChild(notify);
+    }, 1000);
+};
+
+var addUser = function(){
+    let newUser = {
+        name : document.querySelector("#Name-User").value,
+        email : document.querySelector("#Email-User").value,
+        age : document.querySelector("#age-User").value,
+    };
+
+    fetch(`http://89.108.65.123/user/`, {
+        method: 'POST',
+        body : JSON.stringify(newUser)
+    }).then((res) => {
+        notifySucsess();
+    }).catch((err) =>{
+        console.error(err);
+    });
+    console.log(newUser);
+};
 
 window.onload = function () {
-    console.log("hello");
-    var getButton = document.querySelector('#get');
-    getButton.addEventListener('click', testAjax);
+
+    var menuButton = document.querySelector('#getMenu');
+    menuButton.addEventListener('click', getMenu);
+
+    var galeryButton = document.querySelector('#getGalery');
+    galeryButton.addEventListener('click', getGalery);
+
+    var listUserButton = document.querySelector('#getUserList');
+    listUserButton.addEventListener('click', getUserList);
+
+    var idUserButton = document.querySelector('#getUserId');
+    idUserButton.addEventListener('click', getUserId);
+
+    var addUserButton = document.querySelector('#addUser');
+    addUserButton.addEventListener('click', (e) =>{
+        e.preventDefault();
+        addUser()
+    });
+
 };
