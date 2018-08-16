4c5db47623d173a3e750eb1b8feaab513dc968d3
Egior <molodoi_padavan@mail.ru>
2018-08-16 11:35:07 +0300

    Доделана валидация форм

--- Hometask3/common.js
+++ Hometask3/common.js
@@ -9,14 +9,54 @@ var textEdit = function(){
     });
 };
 
+var isValidField = function (field, template) {
+    console.log(`valid ${field.value}`);
+    if (template.test(field.value)){
+        field.classList.remove('invalid');
+        console.log(`valid OK`);
+        return true;
+
+    } else{
+        field.classList.add("invalid");
+        console.log('valid BAD');
+        return false;
+
+    }
+
+};
+
 var validateForm = function(){
 
-    let name = document.querySelector('#name');
-    if ( !(/[a-z]+/gmi.test(name.value))){
-        name.classList.add("invalid");
-    } else{
-        name.classList.remove('invalid');
+    var allValid = true;
+    var name = document.querySelector('#name');
+    var phone = document.querySelector("#phone");
+    var mail = document.querySelector('#email');
+
+    allValid &= isValidField(name,/^[a-zа-яё]+$/i);
+    allValid &= isValidField(phone,/^\+7\(\d{3}\)\d{3}-\d{4}$/);
+    /*Не используется \w потому что включает также _, которое по условию запрещено*/
+    allValid &= isValidField(mail, /^[a-z0-9]+[a-z0-9.-]+@[a-z0-9.-]+\.[a-z0-9]{2,}$/i);
+
+    /*Не использутеся конструкция if (isValidField(name,/[a-z]+/gi) &&
+        isValidField(phone,/^\+7\(\d{3}\)\d{3}-\d{4}$/) &&
+        isValidField(mail, /\w/)
+        Потому что, если хотя бы одно поле на валидно, проверка завершается, следовательно пользователю
+        не будет сообщено обо всех ошибках сразу*/
+
+    if (allValid){
+        name.value ='';
+        phone.value = '';
+        mail.value = '';
+        document.querySelector('#message').value = '';
+        var notify = document.createElement("div");
+        notify.classList.add("notify");
+        notify.innerHTML = "<p>Форма успешно отправлена</p>";
+        document.querySelector('section.feedback').appendChild(notify);
+        setTimeout(()=> {
+            document.querySelector('section.feedback').removeChild(notify)
+        }, 1000);
     }
+
 };
 
 
